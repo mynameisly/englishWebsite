92e7eb0843e40328702eb3c52ef2f765a334d75c
mynameisly <1990505897@qq.com>
2021-01-30 11:59:53 +0800

    delete some files

--- src/api/request.config.js
+++
@@ -1,9 +0,0 @@
-export const REQUEST_BASICS = {
-    baseURL: process.env.NODE_ENV === 'development' ? "/" : "/",
-    maxContentLength: 2000,
-    timeout: 6000,
-    // 发送请求时头部信息
-    headers: {
-        'Content-Type': 'application/json;charset=utf-8'
-    },
-}
