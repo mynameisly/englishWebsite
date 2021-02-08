695ace9b796db452eb5bbbbd36d14bc3cec97bce
mynameisly <1990505897@qq.com>
2021-02-08 21:47:37 +0800

    modify style

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,53 @@
         <router-view></router-view>
     </div>
 </template>
-<style>
+<style lang="less">
     @import "./assets/css/main.css";
     @import "./assets/css/color-dark.css";     /*深色主题*/
     /*@import "./assets/css/theme-green/color-green.css";   浅绿色主题*/
+.panel-intro {
+    .panel-heading {
+        padding: 15px;
+        padding-bottom: 0;
+        background: #e8edf0;
+        border-color: #fff;
+        position: relative;
+        .panel-lead {
+            margin-bottom: 15px;
+            em {
+                display: block;
+                font-weight: bold;
+                font-style: normal;
+            }
+        }
+        .nav-tabs > li > a {
+            margin-right: 4px;
+            color: #95a5a6;
+            background-color: #d8e0e6;
+            border: none;
+            cursor: pointer;
+            border-radius: 0px;
+            border-bottom-color: transparent;
+        }
+        .nav-tabs > li > a:hover {
+            border-bottom-color: transparent;
+        }
+        .nav-tabs > li > a:active {
+            border-bottom-color: transparent;
+        }
+        .nav-tabs > li.active > a {
+            background-color: #fff;
+        }
+    }
+}
 
+// .table-striped > tbody > tr:nth-of-type(odd) {
+//     background-color: #f9f9f9;
+// }
+// .table > thead > tr > th, .table > tbody > tr > th, .table > tfoot > tr > th, .table > thead > tr > td, .table > tbody > tr > td, .table > tfoot > tr > td {
+//     border-top: 1px solid #f4f4f4;
+//     padding: 8px;
+//     line-height: 1.42857143;
+//     vertical-align: top;
+// }
 </style>
--- src/components/page/Dashboard.vue
+++ src/components/page/Dashboard.vue
@@ -4,7 +4,7 @@
             <div class="panel-heading">
                 <div class="panel-lead"><em>控制台</em>用于展示当前系统中的统计数据、统计报表及重要实时数据</div>
                 <ul class="nav nav-tabs">
-                    <li v-for="(item,index) in tabList" :key="index" :class="{active: current == index}" @click="toggleTab(item,index)">
+                    <li v-for="(item, index) in tabList" :key="index" :class="{ active: current == index }" @click="toggleTab(item, index)">
                         <a>{{ item.label }}</a>
                     </li>
                 </ul>
@@ -39,14 +39,14 @@ export default {
                 {
                     label: '自定义',
                     value: 'CustomCom'
-                },
+                }
             ]
         };
     },
     mounted() {},
     methods: {
-        toggleTab(item,index) {
-            console.log('item',item)
+        toggleTab(item, index) {
+            console.log('item', item);
             this.current = index;
             this.currentView = item.value;
         }
@@ -56,21 +56,42 @@ export default {
 
 
 <style lang="less" scoped>
-.panel-intro {
-    .panel-heading {
-        padding: 15px;
-        padding-bottom: 0;
-        background: #e8edf0;
-        border-color: #e8edf0;
-        position: relative;
-        .panel-lead {
-            margin-bottom: 15px;
-            em {
-                display: block;
-                font-weight: bold;
-                font-style: normal;
-            }
-        }
-    }
-}
+// .panel-intro {
+//     .panel-heading {
+//         padding: 15px;
+//         padding-bottom: 0;
+//         background: #e8edf0;
+//         border-color: #fff;
+//         position: relative;
+//         .panel-lead {
+//             margin-bottom: 15px;
+//             em {
+//                 display: block;
+//                 font-weight: bold;
+//                 font-style: normal;
+//             }
+//         }
+//         .nav-tabs > li > a {
+//             margin-right: 4px;
+//             cursor: pointer;
+//             color: #95a5a6;
+//             background-color: #d8e0e6;
+//             border: none;
+//             border-radius: 10px;
+//             border-bottom-color: transparent;
+//         }
+//         .nav-tabs > li > a:hover {
+//             border-bottom-color: transparent;
+//         }
+//         .nav-tabs > li > a:active {
+//             border-bottom-color: transparent;
+//         }
+//         .nav-tabs > li.active > a {
+//             color: #7b8a8b;
+//             background-color: #ffffff;
+//             border-bottom-color: transparent;
+//             cursor: default;
+//         }
+//     }
+// }
 </style>
